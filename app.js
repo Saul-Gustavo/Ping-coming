d0f34232fefdf60851ad637c7e317c0344dc5022
Saul Gustavo <saulgustavo237@gmail.com>
2024-08-20 17:46:05 -0600

    Iniciando repositorio Se agregaron los archivos correspondientes al proyecto

---
+++ app.js
@@ -0,0 +1,24 @@
+const submitBtn = document.querySelector('.submit')
+
+submitBtn.addEventListener('click', () => {
+    const mensaje = document.querySelector('.mensaje');
+    const emailInput = document.querySelector('.input-email');
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailInput.value === "" || !emailRegex.test(emailInput.value)) {
+        mensaje.style.display = "block"
+        mensaje.textContent = "Please provide a valid email address";
+        emailInput.style.border = "1px solid red";
+    } else {
+        mensaje.style.display = "none"
+        mensaje.textContent = ""
+        emailInput.style.border = "1px solid hsl(0, 0%, 59%)";
+    }
+})
+
+
+
+
+
+
+
